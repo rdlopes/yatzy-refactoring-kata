37ce63c783e5780726bb6b79d8eef54a2a42a19b
Rui LOPES <rui.d.lopes@me.com>
2024-05-02 09:16:52 +0200

    refactor: - let Yatzy hold dice values - group individual assertions into parameterized tests

--- src/test/java/org/codingdojo/yatzy/YatzyTest.java
+++ src/test/java/org/codingdojo/yatzy/YatzyTest.java
@@ -1,114 +1,216 @@
 package org.codingdojo.yatzy;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class YatzyTest {
     
-    @Test
-    void chanceScoresSumOfAllDice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2, 3, 4, 5, 1);
-        assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3, 3, 4, 5, 1));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        2, 3, 4, 5, 1, 15
+        3, 3, 4, 5, 1, 16
+        """)
+    void chanceScoresSumOfAllDice(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.chance();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void yatzyScoresFifty() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4, 4, 4, 4, 4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6));
-        assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 3));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        4, 4, 4, 4, 4, 50
+        6, 6, 6, 6, 6, 50
+        6, 6, 6, 6, 3, 0
+        """)
+    void yatzyScoresFifty(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.yatzy();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void onesScoresSumOfOnes() {
-        assertEquals(1, Yatzy.ones(1, 2, 3, 4, 5));
-        assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
-        assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
-        assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        1, 2, 3, 4, 5, 1
+        1, 2, 1, 4, 5, 2
+        6, 2, 2, 4, 5, 0
+        1, 2, 1, 1, 1, 4
+        """)
+    void onesScoresSumOfOnes(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.ones();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void twosScoreSumOfTwos() {
-        assertEquals(4, Yatzy.twos(1, 2, 3, 2, 6));
-        assertEquals(10, Yatzy.twos(2, 2, 2, 2, 2));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        1, 2, 3, 2, 6, 4
+        2, 2, 2, 2, 2, 10
+        """)
+    void twosScoresSumOfTwos(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.twos();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void threesScoreSumOfThrees() {
-        assertEquals(6, Yatzy.threes(1, 2, 3, 2, 3));
-        assertEquals(12, Yatzy.threes(2, 3, 3, 3, 3));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        1, 2, 3, 2, 3, 6
+        2, 3, 3, 3, 3, 12
+        """)
+    void threesScoresSumOfThrees(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.threes();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void foursScoreSumOfFours() {
-        assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
-        assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
-        assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        4, 4, 4, 5, 5, 12
+        4, 4, 5, 5, 5, 8
+        4, 5, 5, 5, 5, 4
+        """)
+    void foursScoresSumOfFours(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.fours();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void fivesScoreSumOfFives() {
-        assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
-        assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
-        assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        4, 4, 4, 5, 5, 10
+        4, 4, 5, 5, 5, 15
+        4, 5, 5, 5, 5, 20
+        """)
+    void fivesScoresSumOfFives(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.fives();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void sixesScoreSumOfSixes() {
-        assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
-        assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
-        assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        4, 4, 4, 5, 5, 0
+        4, 4, 6, 5, 5, 6
+        6, 5, 6, 6, 5, 18
+        """)
+    void sixesScoresSumOfSixes(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.sixes();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void onePairScoresSumOfPairValues() {
-        assertEquals(6, new Yatzy().onePair(3, 4, 3, 5, 6));
-        assertEquals(10, new Yatzy().onePair(5, 3, 3, 3, 5));
-        assertEquals(12, new Yatzy().onePair(5, 3, 6, 6, 5));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        3, 4, 3, 5, 6, 6
+        5, 3, 3, 3, 5, 10
+        5, 3, 6, 6, 5, 12
+        """)
+    void onePairScoresSumOfPair(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.onePair();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void twoPairsScoreSumOfTwoPairsValues() {
-        assertEquals(16, Yatzy.twoPairs(3, 3, 5, 4, 5));
-        assertEquals(16, Yatzy.twoPairs(3, 3, 5, 5, 5));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        3, 3, 5, 4, 5, 16
+        3, 3, 5, 5, 5, 16
+        """)
+    void twoPairsScoresSumOfTwoPairs(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.twoPairs();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void threeOfAKindScoresSumOfThreeOfAKindValues() {
-        assertEquals(9, Yatzy.threeOfAKind(3, 3, 3, 4, 5));
-        assertEquals(15, Yatzy.threeOfAKind(5, 3, 5, 4, 5));
-        assertEquals(9, Yatzy.threeOfAKind(3, 3, 3, 3, 5));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        3, 3, 3, 4, 5, 9
+        5, 3, 5, 4, 5, 15
+        3, 3, 3, 3, 5, 9
+        """)
+    void threeOfAKindScoresSumOfThreeOfAKind(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.threeOfAKind();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void fourOfAKindSumOfFourOfAKindValues() {
-        assertEquals(12, Yatzy.fourOfAKind(3, 3, 3, 3, 5));
-        assertEquals(20, Yatzy.fourOfAKind(5, 5, 5, 4, 5));
-        assertEquals(9, Yatzy.threeOfAKind(3, 3, 3, 3, 3));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        3, 3, 3, 3, 5, 12
+        5, 5, 5, 4, 5, 20
+        3, 3, 3, 3, 3, 12
+        """)
+    void fourOfAKndScoresSumOfFourOfAKind(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.fourOfAKind();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void smallStraightScoresSumOfSmallStraightValues() {
-        assertEquals(15, Yatzy.smallStraight(1, 2, 3, 4, 5));
-        assertEquals(15, Yatzy.smallStraight(2, 3, 4, 5, 1));
-        assertEquals(0, Yatzy.smallStraight(1, 2, 2, 4, 5));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        1, 2, 3, 4, 5, 15
+        2, 3, 4, 5, 1, 15
+        1, 2, 2, 4, 5, 0
+        """)
+    void smallStraightScoresSumOfSmallStraight(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.smallStraight();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void largeStraightScoresSumOfLargeStraightValues() {
-        assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
-        assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
-        assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        6, 2, 3, 4, 5, 20
+        2, 3, 4, 5, 6, 20
+        1, 2, 2, 4, 5, 0
+        """)
+    void largeStraightScoresSumOfLargeStraight(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.largeStraight();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
     
-    @Test
-    void fullHouseScoresSumOfFullHouseValues() {
-        assertEquals(18, Yatzy.fullHouse(6, 2, 2, 2, 6));
-        assertEquals(0, Yatzy.fullHouse(2, 3, 4, 5, 6));
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        6, 2, 2, 2, 6, 18
+        2, 3, 4, 5, 6, 0
+        """)
+    void fullHouseScoresSumOfFullHouse(int dice1, int dice2, int dice3, int dice4, int dice5, int expectedScore) {
+        Yatzy yatzy = new Yatzy(dice1, dice2, dice3, dice4, dice5);
+        
+        int score = yatzy.fullHouse();
+        
+        assertThat(score).isEqualTo(expectedScore);
     }
 }
